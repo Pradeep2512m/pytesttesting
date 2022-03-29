ba71b914db95c8f4d0e82d20099c208f49b64268
pradeep2512m <pradeepm6383@gmail.com>
2022-03-29 10:40:51 +0530

    pytesttesting

--- test_Calculator.py
+++ test_Calculator.py
@@ -1,24 +1,28 @@
 import Calculator
-def test_add2no():
-    x=10
-    y=20
-    result=Calculator.add2no(x, y)
-    assert result == x+y
+import pytest
 
-def test_sub2no():
-    x=10
-    y=20
-    result=Calculator.sub2no(x, y)
-    assert result == x-y
+@pytest.mark.parametrize("a,b,c",[(1,2,3),(4,5,6),(7,8,9)])
+def test_add2no(a,b,c):
 
-def test_mul2no():
-    x=10
-    y=20
-    result=Calculator.mul2no(x, y)
-    assert result == x*y
+    result=Calculator.add2no(a, b)
+    assert result == c
 
-def test_div2no():
-    x=10
-    y=20
-    result=Calculator.div2no(x, y)
-    assert result == x/y
+
+@pytest.mark.parametrize("a,b,c",[(1,2,3),(4,12,6),(7,14,9)])
+def test_sub2no(a,b,c):
+
+    result=Calculator.sub2no(a, b)
+    assert result == c
+
+
+@pytest.mark.parametrize("a,b,c",[(1,2,3),(4,5,6),(7,8,9),(14,12,55)])
+def test_mul2no(a,b,c):
+
+    result=Calculator.mul2no(a, b)
+    assert result == c
+
+@pytest.mark.parametrize("a,b,c",[(1,2,3),(4,5,6),(7,8,9)])
+def test_div2no(a,b,c):
+
+    result=Calculator.div2no(a, b)
+    assert result == c
